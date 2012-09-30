3e5c12ba8daf2a3c469608fa0500763f0e4f7c65
Jarred Li <jiali@vmware.com>
2012-09-30 08:07:47 +0800

    Add sample applications for Splunk adapter

---
+++ samples/splunk/src/main/java/org/springframework/integration/samples/splunk/outbound/SplunkOutboundChannelAdapterStreamSample.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2011-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.samples.splunk.outbound;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.Message;
+import org.springframework.integration.core.SubscribableChannel;
+import org.springframework.integration.splunk.entity.SplunkData;
+import org.springframework.integration.support.MessageBuilder;
+
+
+/**
+ * @author Jarred Li
+ * @since 1.0
+ *
+ */
+public class SplunkOutboundChannelAdapterStreamSample {
+
+	public static void main(String args[]) {
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"SplunkOutboundChannelAdapterStreamTests-context.xml", SplunkOutboundChannelAdapterStreamSample.class);
+		ctx.start();
+
+		SubscribableChannel channel = ctx.getBean("outputToSplunk", SubscribableChannel.class);
+
+		SplunkData data = new SplunkData("spring", "spring:example");
+		data.setCommonDesc("description");
+
+		Message<SplunkData> msg = MessageBuilder.withPayload(data).build();
+		channel.send(msg);
+
+
+	}
+}
