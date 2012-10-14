2034dbb6c88967b3d459475e1dc9c0c80bc01642
Gary Russell <grussell@vmware.com>
2012-10-14 13:50:43 -0400

    INTEXT-24 Lightweight WebSocket Server
    * Add Lightweight WebSocket Server Support
    * Run WebSocketServerTests and open ws.html in a browser.
      - Sending 'start' begins sending an incrementing # once per second.
      - 'stop' stops the stream (leaving the socket open), 'start' resumes again.
      - Test terminates after 60 seconds.
    * Fixes and Improvements for WebSocket Server
      - fix masking
      - fix bytes sent
      - add error handling to remove dead sockets
      - create new web page instead of using vert.x example
      - add status box
      - automatically update message to send to start/stop appropriately
      - update SI to 2.2.0.RELEASE
      - change test to a main()
    * Use Interceptor for Handshake
    * Move handshake to an interceptor instead of doing it in the SI flow.
    * Add close button to web page.
    * Add code to remove state from deserializer on close.
    * Implement Orderly Close Per RFC6455
    * Rename Packages - org.springframework.integration.x.*
    * Change Version to 0.1.0.
    * Autbahn Test Suite - All Tests Pass
      - 1.1.x tests from the Autobahn Test Suite.
      - Autobahn 1.2.x Tests (Binary)
      - Autobahn Test 2.* (Ping/Pong)
      - Autobahn Tests 3.* (Reserved Bits)
      - Autobahn Tests 4.* (Invalid Opcodes)
      - Autobahn 5.* (Fragmentation)
      - Autobahn 6.* (UTF-8 Handling)
      - Autobahn Tests 7.* (Close Handling)
    * "non-strict" results - fast fail on bad UTF-8. We currently don't detect the bad UTF-8 until all fragments are received.
    * Run AutobahnTests.java and add the following to fuzzingclient.json...
    
        {"agent": "SIServer", "url": "ws://localhost:18080", "options": {"version": 18}}
    
    * Remove sysout
    * Replace with logger.debug().
    * Remove SockJS Dependencies
      - Project started as a SockJS client; WebSocket classes were incorrectly dependent on some SockJS code.
    * Extracted the WebSocket code to its own class hierarchy.
    * Test Autobahn with SSL (wss://...)
      - Add trust store and key store.
      - Add config to listen for wss: as well as ws: connections
    * To test Autobahn for both ws and wss - In the wstest config file use:
    
        "servers": [
            {"agent": "SIServer", "url": "ws://localhost:18080", "options": {"version": 18}},
            {"agent": "SIServerSSL", "url": "wss://localhost:28080", "options": {"version": 18}}
                  ],
    
    * Polishing + Resequence when Using NIO
      - Improve debug logging
      - Clear fragments
      - Add ResequencingMessageHandler to resequence messages when using NIO (frames can arrive on different threads, out of order; this causes issues with the Autobahn tests.
    * Reject Non WS Connections
      - only apply Resequencer if NIO Connection factory is configured with apply-sequence.
      - check protocol version - only 13 is supported.

---
+++ spring-integration-ip-extensions/src/main/java/org/springframework/integration/x/ip/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Root package of the IP Extensions.
+ */
+package org.springframework.integration.x.ip;
---
+++ spring-integration-ip-extensions/src/main/java/org/springframework/integration/x/ip/serializer/AbstractByteArraySerializer.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.x.ip.serializer;
+
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.core.serializer.Deserializer;
+import org.springframework.core.serializer.Serializer;
+
+/**
+ * Base class for (de)serializers that provide a mechanism to
+ * reconstruct a byte array from an arbitrary stream.
+ *
+ * TODO: Enhanced version of standard class - will be merged in 3.0.
+ *
+ * @author Gary Russell
+ * @since 2.0
+ *
+ */
+public abstract class AbstractByteArraySerializer implements
+		Serializer<byte[]>,
+		Deserializer<byte[]> {
+
+	protected int maxMessageSize = 2048;
+
+	protected final Log logger = LogFactory.getLog(this.getClass());
+
+	/**
+	 * The maximum supported message size for this serializer.
+	 * Default 2048.
+	 * @return The max message size.
+	 */
+	public int getMaxMessageSize() {
+		return maxMessageSize;
+	}
+
+	/**
+	 * The maximum supported message size for this serializer.
+	 * Default 2048.
+	 * @param maxMessageSize The max message size.
+	 */
+	public void setMaxMessageSize(int maxMessageSize) {
+		this.maxMessageSize = maxMessageSize;
+	}
+
+	protected void checkClosure(int bite) throws IOException {
+		if (bite < 0) {
+			logger.debug("Socket closed during message assembly");
+			throw new IOException("Socket closed during message assembly");
+		}
+	}
+
+	/**
+	 * Copy size bytes to a new buffer exactly size bytes long.
+	 * @param buffer The buffer containing the data.
+	 * @param size The number of bytes to copy.
+	 * @return The new buffer, or the buffer parameter if it is
+	 * already the correct size.
+	 */
+	protected byte[] copyToSizedArray(byte[] buffer, int size) {
+		if (size == buffer.length) {
+			return buffer;
+		}
+		byte[] assembledData = new byte[size];
+		System.arraycopy(buffer, 0, assembledData, 0, size);
+		return assembledData;
+	}
+
+}
