3931ae58269876f99893080a15d981c03ccf5a0b
jpalka <jpalka@gmail.com>
2013-05-15 22:38:08 +0200

    INTEXT-65 Add JGroups Extension

---
+++ spring-integration-jgroups/src/main/java/org/springframework/integration/jgroups/config/JGroupsClusterAdapterParser.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2013 Jaroslaw Palka<jaroslaw.palka@symentis.pl>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.jgroups.config;
+
+import org.springframework.beans.factory.BeanDefinitionStoreException;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.AbstractBeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.integration.jgroups.JGroupsChannelFactoryBean;
+import org.springframework.integration.jgroups.XmlConfiguratorFactoryBean;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+/**
+ *
+ * @author Jaroslaw Palka <jaroslaw.palka@symentis.pl>
+ * @since 1.0.0
+ *
+ */
+public class JGroupsClusterAdapterParser extends AbstractBeanDefinitionParser {
+
+	@Override
+	protected AbstractBeanDefinition parseInternal(Element element, ParserContext parserContext) {
+
+		BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(JGroupsChannelFactoryBean.class).addPropertyValue(
+				"clusterName", element.getAttribute("name"));
+
+		// TODO this is quick and dirty as I don't still get how to work with
+		// root and child beans parsing
+		NodeList nodeList = element.getElementsByTagName("jgroups:xml-configurator");
+		if (nodeList.getLength() == 1) {
+			Element configuratorElement = (Element) nodeList.item(0);
+
+			AbstractBeanDefinition beanDefinition = BeanDefinitionBuilder.genericBeanDefinition(XmlConfiguratorFactoryBean.class)
+					.addPropertyValue("resource", configuratorElement.getAttribute("resource")).getBeanDefinition();
+			builder.addPropertyValue("protocolStackConfigurator", beanDefinition);// }
+		}
+
+		return builder.getBeanDefinition();
+	}
+
+	/**
+	 * Resolves bean ID to cluster name
+	 */
+	@Override
+	protected String resolveId(Element element, AbstractBeanDefinition definition, ParserContext parserContext)
+			throws BeanDefinitionStoreException {
+		return element.getAttribute("name");
+	}
+}
+
---
+++ spring-integration-jgroups/src/test/java/org/springframework/integration/jgroups/JGroupsInboundEndpointTest.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2013 Jaroslaw Palka<jaroslaw.palka@symentis.pl>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.jgroups;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.jgroups.JChannel;
+import org.jgroups.Receiver;
+import org.junit.Test;
+
+/**
+ *
+ * @author Jaroslaw Palka <jaroslaw.palka@symentis.pl>
+ * @since 1.0.0
+ *
+ */
+public class JGroupsInboundEndpointTest {
+
+	@Test
+	public void should_set_receiver_on_endpoint_start() {
+
+		JChannel jgroupsChannel = mock(JChannel.class);
+		JGroupsInboundEndpoint inboundEndpoint = new JGroupsInboundEndpoint(jgroupsChannel);
+
+		inboundEndpoint.start();
+
+		verify(jgroupsChannel).setReceiver(any(Receiver.class));
+
+	}
+}
+
