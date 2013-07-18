933e8b5a9ffa49d9e1a972d3f8feef8744e574a9
Rajasekar Elango <relango@saleforce.com>
2013-07-18 12:53:46 -0700

    Added test cases for consumer topic filter

--- spring-integration-kafka/src/test/java/org/springframework/integration/kafka/support/ConsumerConfigurationTests.java
+++ spring-integration-kafka/src/test/java/org/springframework/integration/kafka/support/ConsumerConfigurationTests.java
@@ -224,6 +224,8 @@ public class ConsumerConfigurationTests {
 
 		Assert.assertEquals(sum, 9);
 	}
+	
+	
 
 	private Answer<Object> getAnswer() {
 		return new Answer<Object>() {
@@ -306,6 +308,114 @@ public class ConsumerConfigurationTests {
 		Assert.assertTrue(valueFound(messages.get("topic1").get(1), "value1"));
 		Assert.assertTrue(valueFound(messages.get("topic2").get(1), "value2"));
 		Assert.assertTrue(valueFound(messages.get("topic3").get(1), "value3"));
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	public void testReceiveMessageForTopicFilterFromSingleStream() {
+		final ConsumerMetadata consumerMetadata = Mockito.mock(ConsumerMetadata.class);
+		final ConsumerConnectionProvider consumerConnectionProvider =
+				Mockito.mock(ConsumerConnectionProvider.class);
+		final MessageLeftOverTracker messageLeftOverTracker = Mockito.mock(MessageLeftOverTracker.class);
+		final ConsumerConnector consumerConnector = Mockito.mock(ConsumerConnector.class);
+
+		Mockito.when(consumerMetadata.getTopicFilterConfiguration()).thenReturn(new TopicFilterConfiguration(".*", 1, false));
+		
+		Mockito.when(consumerConnectionProvider.getConsumerConnector()).thenReturn(consumerConnector);
+
+		final ConsumerConfiguration consumerConfiguration = new ConsumerConfiguration(consumerMetadata,
+				consumerConnectionProvider, messageLeftOverTracker);
+		consumerConfiguration.setMaxMessages(1);
+
+		final KafkaStream stream = Mockito.mock(KafkaStream.class);
+		final List<KafkaStream<byte[], byte[]>> streams = new ArrayList<KafkaStream<byte[], byte[]>>();
+		streams.add(stream);
+		
+		Mockito.when(consumerConfiguration.createMessageStreamsForTopicFilter()).thenReturn(streams);
+		final ConsumerIterator iterator = Mockito.mock(ConsumerIterator.class);
+		Mockito.when(stream.iterator()).thenReturn(iterator);
+		final MessageAndMetadata messageAndMetadata = Mockito.mock(MessageAndMetadata.class);
+		Mockito.when(iterator.next()).thenReturn(messageAndMetadata);
+		Mockito.when(messageAndMetadata.message()).thenReturn("got message");
+		Mockito.when(messageAndMetadata.topic()).thenReturn("topic");
+		Mockito.when(messageAndMetadata.partition()).thenReturn(1);
+
+		final Map<String, Map<Integer, List<Object>>> messages = consumerConfiguration.receive();
+		Assert.assertEquals(messages.size(), 1);
+		Assert.assertEquals(messages.get("topic").size(), 1);
+		Assert.assertEquals(messages.get("topic").get(1).get(0), "got message");
+
+		Mockito.verify(stream, Mockito.times(1)).iterator();
+		Mockito.verify(iterator, Mockito.times(1)).next();
+		Mockito.verify(messageAndMetadata, Mockito.times(1)).message();
+		Mockito.verify(messageAndMetadata, Mockito.times(1)).topic();
+	}
+	
+	@Test
+	@SuppressWarnings("unchecked")
+	public void testReceiveMessageForTopicFilterFromMultipleStreams() {
+		final ConsumerMetadata consumerMetadata = Mockito.mock(ConsumerMetadata.class);
+		final ConsumerConnectionProvider consumerConnectionProvider =
+				Mockito.mock(ConsumerConnectionProvider.class);
+		final MessageLeftOverTracker messageLeftOverTracker = Mockito.mock(MessageLeftOverTracker.class);
+
+		Mockito.when(consumerMetadata.getTopicFilterConfiguration()).thenReturn(new TopicFilterConfiguration(".*", 1, false));
+		
+		final ConsumerConnector consumerConnector = Mockito.mock(ConsumerConnector.class);
+
+		Mockito.when(consumerConnectionProvider.getConsumerConnector()).thenReturn(consumerConnector);
+
+		final ConsumerConfiguration consumerConfiguration = new ConsumerConfiguration(consumerMetadata,
+				consumerConnectionProvider, messageLeftOverTracker);
+		consumerConfiguration.setMaxMessages(3);
+
+		final KafkaStream stream1 = Mockito.mock(KafkaStream.class);
+		final KafkaStream stream2 = Mockito.mock(KafkaStream.class);
+		final KafkaStream stream3 = Mockito.mock(KafkaStream.class);
+		final List<KafkaStream<byte[], byte[]>> streams = new ArrayList<KafkaStream<byte[], byte[]>>();
+		streams.add(stream1);
+		streams.add(stream2);
+		streams.add(stream3);
+		
+		Mockito.when(consumerConfiguration.createMessageStreamsForTopicFilter()).thenReturn(streams);
+		final ConsumerIterator iterator1 = Mockito.mock(ConsumerIterator.class);
+		final ConsumerIterator iterator2 = Mockito.mock(ConsumerIterator.class);
+		final ConsumerIterator iterator3 = Mockito.mock(ConsumerIterator.class);
+
+		Mockito.when(stream1.iterator()).thenReturn(iterator1);
+		Mockito.when(stream2.iterator()).thenReturn(iterator2);
+		Mockito.when(stream3.iterator()).thenReturn(iterator3);
+		final MessageAndMetadata messageAndMetadata1 = Mockito.mock(MessageAndMetadata.class);
+		final MessageAndMetadata messageAndMetadata2 = Mockito.mock(MessageAndMetadata.class);
+		final MessageAndMetadata messageAndMetadata3 = Mockito.mock(MessageAndMetadata.class);
+
+		Mockito.when(iterator1.next()).thenReturn(messageAndMetadata1);
+		Mockito.when(iterator2.next()).thenReturn(messageAndMetadata2);
+		Mockito.when(iterator3.next()).thenReturn(messageAndMetadata3);
+
+		Mockito.when(messageAndMetadata1.message()).thenReturn("got message");
+		Mockito.when(messageAndMetadata1.topic()).thenReturn("topic");
+		Mockito.when(messageAndMetadata1.partition()).thenReturn(1);
+
+		Mockito.when(messageAndMetadata2.message()).thenReturn("got message");
+		Mockito.when(messageAndMetadata2.topic()).thenReturn("topic");
+		Mockito.when(messageAndMetadata2.partition()).thenReturn(2);
+
+		Mockito.when(messageAndMetadata3.message()).thenReturn("got message");
+		Mockito.when(messageAndMetadata3.topic()).thenReturn("topic");
+		Mockito.when(messageAndMetadata3.partition()).thenReturn(3);
+
+		final Map<String, Map<Integer, List<Object>>> messages = consumerConfiguration.receive();
+		Assert.assertEquals(messages.size(), 1);
+		int sum = 0;
+
+		final Map<Integer, List<Object>> values = messages.get("topic");
+
+		for (final List<Object> l : values.values()) {
+			sum += l.size();
+		}
+
+		Assert.assertEquals(sum, 3);
 	}
 
 	private boolean valueFound(final List<Object> l, final String value){
