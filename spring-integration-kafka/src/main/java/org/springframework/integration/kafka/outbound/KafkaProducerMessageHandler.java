44e7a3ccce2bd42d11917a2a2e798ee7dc058a8a
Artem Bilan <abilan@pivotal.io>
2014-11-13 14:35:02 +0200

    IE-100: Kafka:o-c-a: topic and message-key attrs
    JIRA: https://jira.spring.io/browse/INTEXT-100
    
    Add `topic(-expression)` and `message-key(-expression)` attributes to avoid upstream configuration
    to specify them in the `MessageHeaders`
    
    Polishing for XSD
    
    Conflicts:
    	spring-integration-kafka/src/main/java/org/springframework/integration/kafka/support/KafkaProducerContext.java
    	spring-integration-kafka/src/main/java/org/springframework/integration/kafka/support/ProducerConfiguration.java
    	spring-integration-kafka/src/main/resources/org/springframework/integration/config/xml/spring-integration-kafka-1.0.xsd
    	spring-integration-kafka/src/test/java/org/springframework/integration/kafka/config/xml/KafkaOutboundAdapterParserTests.java
    
    INTEXT-100: Add `Assert.notNull(this.evaluationContext);`
    
    INTEXT-100: Fix parser potential NPE
    
    INTEXT-100: Add `KafkaHeaders`

--- spring-integration-kafka/src/main/java/org/springframework/integration/kafka/outbound/KafkaProducerMessageHandler.java
+++ spring-integration-kafka/src/main/java/org/springframework/integration/kafka/outbound/KafkaProducerMessageHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2013-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,30 +13,71 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.integration.kafka.outbound;
 
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.integration.expression.IntegrationEvaluationContextAware;
 import org.springframework.integration.handler.AbstractMessageHandler;
+import org.springframework.integration.kafka.support.KafkaHeaders;
 import org.springframework.integration.kafka.support.KafkaProducerContext;
 import org.springframework.messaging.Message;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 /**
  * @author Soby Chacko
+ * @author Artem Bilan
  * @since 0.5
  */
-public class KafkaProducerMessageHandler<K,V> extends AbstractMessageHandler {
+public class KafkaProducerMessageHandler<K,V> extends AbstractMessageHandler
+		implements IntegrationEvaluationContextAware {
 
 	private final KafkaProducerContext<K,V> kafkaProducerContext;
+
+	private EvaluationContext evaluationContext;
+
+	private volatile Expression topicExpression;
+
+	private volatile Expression messageKeyExpression;
 
 	public KafkaProducerMessageHandler(final KafkaProducerContext<K,V> kafkaProducerContext) {
 		this.kafkaProducerContext = kafkaProducerContext;
 	}
 
+	@Override
+	public void setIntegrationEvaluationContext(EvaluationContext evaluationContext) {
+		this.evaluationContext = evaluationContext;
+	}
+
+	public void setTopicExpression(Expression topicExpression) {
+		this.topicExpression = topicExpression;
+	}
+
+	public void setMessageKeyExpression(Expression messageKeyExpression) {
+		this.messageKeyExpression = messageKeyExpression;
+	}
+
 	public KafkaProducerContext<K,V> getKafkaProducerContext() {
-		return kafkaProducerContext;
+		return this.kafkaProducerContext;
+	}
+
+	@Override
+	protected void onInit() throws Exception {
+		Assert.notNull(this.evaluationContext);
 	}
 
 	@Override
 	protected void handleMessageInternal(final Message<?> message) throws Exception {
-		kafkaProducerContext.send(message);
+		String topic = this.topicExpression != null
+				? this.topicExpression.getValue(this.evaluationContext, message, String.class)
+				: message.getHeaders().get(KafkaHeaders.TOPIC, String.class);
+		Assert.state(StringUtils.hasText(topic), "'topic' can't be evaluated to empty.");
+		Object messageKey = this.messageKeyExpression != null
+				? this.messageKeyExpression.getValue(this.evaluationContext, message)
+				: message.getHeaders().get(KafkaHeaders.MESSAGE_KEY);
+		this.kafkaProducerContext.send(topic, messageKey, message);
 	}
+
 }
