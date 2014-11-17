6b37a400d928344aa9fed3260ca539cc4e892a34
Artem Bilan <abilan@pivotal.io>
2014-11-17 12:26:59 +0200

    Move Splunk to the individual repo

--- spring-integration-splunk/src/main/java/org/springframework/integration/splunk/core/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides core classes of the Splunk module.
- */
-package org.springframework.integration.splunk.core;
--- spring-integration-splunk/src/main/java/org/springframework/integration/splunk/inbound/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides inbound Spring Integration Splunk components.
- */
-package org.springframework.integration.splunk.inbound;
