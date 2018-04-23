10649fa16b85ee1838a7c99881cce93f149b25aa
Artem Bilan <abilan@pivotal.io>
2018-04-23 14:57:16 -0400

    INT-4453: Move SI Twitter module to Extensions
    JIRA: https://jira.spring.io/browse/INT-4453
    
    Since Spring Social project is aiming to its EOL, there is no reason
    to keep such a dependency in the Core project
    
    * Create a `spring-integration-social-twitter` project
    * Move all the sources and tests from the SI Core to this project
    * Rename `spring-integration-twitter-5.1.xsd` to `spring-integration-social-twitter-1.0.xsd`
    do not confuse IDE for auto imports of XSDs
    * Move `twitter.adoc` to README of this extension project
    * Replace `RedisMetadaStore` in test-case to a `SimpleMetadataStore` since
    this integration test does nothing with Redis and we don't have access to the
    `@RedisAvailable` JUnit rule
    * Enable all the Checkstyle rules and fix violations

---
+++ spring-integration-social-twitter/src/main/java/org/springframework/integration/twitter/core/TwitterHeaders.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.twitter.core;
+
+/**
+ * Header keys used by the various Twitter adapters.
+ *
+ * @author Josh Long
+ * @author Oleg Zhurakousky
+ * @author Gary Russell
+ * @author Artem Bilan
+ */
+public final class TwitterHeaders {
+
+	private static final String PREFIX = "twitter_";
+
+	/**
+	 * The name of the header for the target user for DM.
+	 */
+	public static final String DM_TARGET_USER_ID = PREFIX + "dmTargetUserId";
+
+	/**
+	 * The name of the header for the search metadata.
+	 */
+	public static final String SEARCH_METADATA = PREFIX + "searchMetadata";
+
+	private TwitterHeaders() { }
+
+}
